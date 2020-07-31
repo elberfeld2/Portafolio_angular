684e444fb6541a328815546501b28a84917f453a
elberfeld2 <enrique5001@hotmail.com>
2020-07-31 04:21:55 +0000

    Firebase

--- portafolio/src/app/services/info-pagina.service.ts
+++ portafolio/src/app/services/info-pagina.service.ts
@@ -8,9 +8,14 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info:InfoPagina = {};
   cargada = false;
+  equipo:any;
 
   constructor(private http:HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo(){
     this.http.get("assets/data/data-pagina.json")
       .subscribe((resp:InfoPagina)=>{
         this.cargada = true;
@@ -18,4 +23,12 @@ export class InfoPaginaService {
         console.log(resp);
       });
   }
+
+  private cargarEquipo(){
+    this.http.get("https://angular-html-1bb9a.firebaseio.com/equipo.json")
+      .subscribe((resp:any)=>{
+        this.equipo = resp;
+      });
+  }
+
 }
---
+++ portafolio/src/app/services/productos.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+  cargando=true;
+  productos:Producto[] = [];
+  constructor(public http:HttpClient) {
+    this.cargarProductos();
+  }
+
+
+  private cargarProductos(){
+    this.http.get("https://angular-html-1bb9a.firebaseio.com/productos_idx.json")
+      .subscribe((resp:Producto[])=>{
+        this.cargando=false;
+        this.productos=resp;
+      });
+  }
+}
